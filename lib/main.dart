e1d21c2c23f8632869c53143cd8ba0a0050f8d10
ismail shah <smily124000@gmail.com>
2021-10-01 19:52:43 +0500

    updated with gridview

--- lib/main.dart
+++ lib/main.dart
@@ -39,31 +39,47 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text('Material App Bar'),
           ),
-          body: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                  leading: Icon(Icons.image),
-                  title: Text(items[index]),
-                  trailing: Icon(Icons.image),
-                  onTap: () {
-                    Route route = MaterialPageRoute(
-                      builder: (context) => NamedWidget(
-                        name: items[index],
+          body: GridView.builder(
+              shrinkWrap: true,
+              itemCount: 8,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 5,
+                              color: Colors.grey,
+                              spreadRadius: 5,
+                              offset: Offset(-3, 3))
+                        ]),
+                    width: 100,
+                    height: 100,
+                    child: Center(
+                      child: Text(
+                        index.toString(),
                       ),
-                    );
-                    Navigator.of(context).push(route);
-                  });
-            },
-          ),
+                    ),
+                  ),
+                );
+              }),
         ));
   }
 }
+
 class AnyWidget extends StatelessWidget {
-  const AnyWidget({ Key? key }) : super(key: key);
+  const AnyWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(color:Colors.pink),);
+    return Scaffold(
+      body: Container(color: Colors.pink),
+    );
   }
 }
